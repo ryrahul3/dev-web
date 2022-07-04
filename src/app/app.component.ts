17529a964fdeaaf6d6c720d3d2e54f28dc9f15a3
Rahul Yadav <ryrahul_yadav@outlook.com>
2022-07-05 00:00:35 +0530

    fix landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { Location } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dev-web';
-  constructor(private location: Location) {}
+export class AppComponent implements OnInit {
+  private _router!: Subscription;
+  @ViewChild(NavbarComponent)
+  navbar!: NavbarComponent;
+
+  constructor(
+    private location: Location,
+    private router: Router,
+    private renderer: Renderer2,
+    private element: ElementRef,
+    @Inject(DOCUMENT) private document: any
+  ) {}
+  ngOnInit(): void {
+    var navbar: HTMLElement =
+      this.element.nativeElement.children[0].children[0];
+    this._router = this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (window.outerWidth > 991) {
+          window.document.children[0].scrollTop = 0;
+        } else {
+          window.document.activeElement!.scrollTop = 0;
+        }
+        this.navbar.sidebarClose();
+      });
+    this.renderer.listen('window', 'scroll', (event) => {
+      const number = window.scrollY;
+      if (number > 150 || window.pageYOffset > 150) {
+        // add logic
+        navbar.classList.remove('navbar-transparent');
+      } else {
+        // remove logic
+        navbar.classList.add('navbar-transparent');
+      }
+    });
+    var ua = window.navigator.userAgent;
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      if (version) {
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('ie-background');
+      }
+    }
+  }
   removeFooter() {
     var titlee = this.location.prepareExternalUrl(this.location.path());
     titlee = titlee.slice(1);
